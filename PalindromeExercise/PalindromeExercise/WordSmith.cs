43637106bdf17c41580b864f8f7847652f832f29
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-02-15 12:29:41 -0500

    redo complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if(word == null || word.Length == 0)
+            {
+                return false;
+            }
+
+            string wordLowered = word.ToLower();
+            string reversedString = "";
+
+            for(int i = word.Length - 1; i >= 0; i--)
+            {
+                reversedString += wordLowered[i];
+            }
+
+            if (wordLowered == reversedString)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,26 @@
+using PalindromeExercise;
 using System;
 using Xunit;
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("hello", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+
+        public void Test1(string word, bool expected)
         {
+
+            var w = new WordSmith();
+            var actual = w.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
 
         }
     }
